5b95bb3ed4b0ab94983394c1263cb4ff6e0c8599
Jack Sipley <sipleyjack@gmail.com>
2025-08-03 21:09:11 -0400

    Refined pascals triangle solution with syntactic sugar

--- src/solns/pascals_triangle.rs
+++ src/solns/pascals_triangle.rs
@@ -1,24 +1,20 @@
 
 pub fn generate(num_rows: i32) -> Vec<Vec<i32>>
 {
-    let mut result: Vec<Vec<i32>> = Vec::new();
-    if num_rows == 0 {
-        return result;
-    }
-    result.push(vec![1]);
-    for r in 1..num_rows as usize
+    let mut triangle = Vec::with_capacity(num_rows as usize);
+    for r in 0..num_rows as usize 
     {
-        let mut row: Vec<i32> = Vec::new();
+        let mut row = Vec::with_capacity(r + 1);
         row.push(1);
-        for c in 1..r
+        if r > 0 
         {
-            let value = result[r - 1][c - 1] + result[r - 1][c];
-            row.push(value);
+            let prev:&Vec<i32> = &triangle[r - 1];
+            row.extend(prev.windows(2).map(|w| w[0] + w[1]));
+            row.push(1);
         }
-        row.push(1);
-        result.push(row);
+        triangle.push(row);
     }
-    return result;
+    triangle
 }
 
 pub fn run()
@@ -29,27 +25,3 @@ pub fn run()
         println!("{:?}", row);
     }
 }
-
-
-// C# impl as reference
-
-// static IList<IList<int>> Generate(int numRows)
-// {
-//     IList<IList<int>> result = new List<IList<int>>();
-//     List<int> firstRow = new List<int> { 1 };
-//     result.Add(firstRow);
-//     for (int r = 1; r < numRows; r++)
-//     {
-//         IList<int> row = new List<int>();
-//         row.Add(1); // First element is always 1
-//         for (int c = 1; c < r; c++)
-//         {
-//             // Each triangle element is the sum of the two elements above it
-//             int value = ((IList<int>)result[r - 1])[c - 1] + ((IList<int>)result[r - 1])[c];
-//             row.Add(value);
-//         }
-//         row.Add(1); // Last element is always 1
-//         result.Add(row);
-//     }
-//     return result;
-// }
