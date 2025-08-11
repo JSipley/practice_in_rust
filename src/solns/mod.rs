45f95eeae4471c2a5d814bb0ab58a6d0a057c144
Jack Sipley <sipleyjack@gmail.com>
2025-08-10 23:02:09 -0400

    Added dead_code attribute to supress warnings from compiler

--- src/solns/mod.rs
+++ src/solns/mod.rs
@@ -1,5 +1,7 @@
 // Every module is in its own file. Modules are used to call into
 // main program.
 
+#![allow(dead_code)] //Ignore warnings for previous code that isn't called
+
 pub mod pascals_triangle;
 pub mod reverse_integer;
