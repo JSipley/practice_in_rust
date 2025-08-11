88a7b103057604b0192a5f94b861545b24fec390
Jack Sipley <sipleyjack@gmail.com>
2025-08-10 23:03:09 -0400

    reverse integer solution

--- src/solns/reverse_integer.rs
+++ src/solns/reverse_integer.rs
@@ -1,7 +1,19 @@
 
 pub fn reverse(x: i32) -> i32
 {
-    -1
+    let mut result: i32 = 0;
+    let mut num = x;
+    while num != 0 {
+        let digit = num % 10;
+        if let Some(new_result) = result.checked_mul(10)
+                                             .and_then(|r| r.checked_add(digit)) {
+            result = new_result;
+        } else {
+            return 0;
+        }
+        num /= 10;
+    }
+    result
 }
 
 pub fn run()
